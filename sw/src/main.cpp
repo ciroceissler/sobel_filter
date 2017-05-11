97392e4716f19a4c39a7525a45ec571f0b859ff4
ciroceissler <ciro.ceissler@gmail.com>
2017-05-11 01:16:46 -0300

    initial CMake configuration

---
+++ sw/src/main.cpp
@@ -0,0 +1,17 @@
+//===----- main.cpp - load png file and execute Sobel Filter --------------===//
+//
+// Copyright (c) 2017 Ciro Ceissler
+//
+// See LICENSE for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file load png file with libpng, execute Sobel Filter
+// detect edges and generate image file.
+//
+//===----------------------------------------------------------------------===//
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
+
