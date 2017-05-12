f6233828e9407d59cb665c788563a5558f2298ed
ciroceissler <ciro.ceissler@gmail.com>
2017-05-12 15:55:15 -0300

    code refactor rgb2luma

--- hw/rtl/rgb2luma.sv
+++ hw/rtl/rgb2luma.sv
@@ -29,47 +29,44 @@ module rgb2luma
 
   reg [2:0] color;
 
-  always_ff@(posedge clk or posedge rst) begin : GRAYSCALE
-    if (rst) begin : RESET
-      data_out  <= 8'h00;
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      data_out <= 8'h00;
+    end
+    else if (valid_in) begin
+      case (color)
+        RED    : data_out <= data_in*0.299;
+        GREEN  : data_out <= data_out + data_in*0.587;
+        BLUE   : data_out <= data_out + data_in*0.114;
+      endcase
+    end
+  end
+
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      color <= RED;
+    end
+    else if (valid_in) begin
+      case (color)
+        RED    : color <= GREEN;
+        GREEN  : color <= BLUE;
+        BLUE   : color <= RED;
+        default: color <= RED;
+      endcase
+    end
+  end
+
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
       valid_out <= 1'b0;
-      color     <= RED;
-    end : RESET
-    else begin : NORMAL_FLOW
-      if (valid_in) begin : VALID
-        case (color)
-          RED:
-            begin
-              data_out  <= data_in*0.299;
-              valid_out <= 1'b0;
-              color     <= GREEN;
-            end
-
-          GREEN:
-            begin
-              data_out  <= data_out + data_in*0.587;
-              valid_out <= 1'b0;
-              color     <= BLUE;
-            end
-
-          BLUE:
-            begin
-              data_out  <= data_out + data_in*0.114;
-              valid_out <= 1'b1;
-              color     <= RED;
-            end
-
-          default:
-            begin
-              valid_out <= 1'b0;
-            end
-        endcase
-      end : VALID
-      else begin : NOT_VALID
-        valid_out <= 1'b0;
-      end : NOT_VALID
-    end : NORMAL_FLOW
-  end : GRAYSCALE
+    end
+    else begin
+      case (color)
+        BLUE   : valid_out <= valid_in;
+        default: valid_out <= 1'b0;
+      endcase
+    end
+  end
 
 endmodule : rgb2luma
 
